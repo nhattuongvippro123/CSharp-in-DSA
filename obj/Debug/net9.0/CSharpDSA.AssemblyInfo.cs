1e4b798e5a182b29af5d9731d9705e3086c05778
nhattuongvippro123 <tuongdan160399@gmail.com>
2025-05-04 13:47:35 +0700

    add file gitignore

--- obj/Debug/net9.0/CSharpDSA.AssemblyInfo.cs
+++ obj/Debug/net9.0/CSharpDSA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpDSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5600228b689f0ba0571880b68fe09c9e296a3fee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbbf0201440c013d01c13c5c6ee4fcc3d68451fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpDSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpDSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
